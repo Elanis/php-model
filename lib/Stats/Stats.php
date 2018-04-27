79d89ff2c048392f4d9c48de2dcf7d2bee3a68cb
Elanis <elanis@hotmail.com>
2018-04-27 21:23:29 +0200

    Added function full_url to Web Class

--- lib/Stats/Stats.php
+++ lib/Stats/Stats.php
@@ -1,24 +1,4 @@
 <?php
-/**
- * Get the current full URL
- *
- * @param      <type>   $s                   { parameter_description }
- * @param      boolean  $use_forwarded_host  The use forwarded host
- *
- * @return     string   ( description_of_the_return_value )
- */
-function full_url( $s, $use_forwarded_host = false )
-{
-    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
-    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
-    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
-    $port     = $s['SERVER_PORT'];
-    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
-    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
-    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
-    return $protocol . '://' . $host . $s['REQUEST_URI'];
-}
-
 abstract class Stats extends MongoInterface {
     static function newVisit() {
         if(!isset($_SESSION['visited']) || $_SESSION['visited']!=$_SERVER['REQUEST_URI']) {
@@ -37,7 +17,7 @@ abstract class Stats extends MongoInterface {
             }
             
             $visit["ip"] = Web::get_client_ip();
-            $visit["url"] = full_url($_SERVER);
+            $visit["url"] = Web::get_full_url($_SERVER);
             $visit["origin"] = (isset($_SERVER['HTTP_REFERER']))?$_SERVER['HTTP_REFERER']:null;
             $visit["time"] = time();
 
--- lib/Web/Web.php
+++ lib/Web/Web.php
@@ -92,4 +92,23 @@ abstract class Web {
 	        $ipaddress = 'UNKNOWN';
 	    return $ipaddress;
 	}
+
+	/**
+	 * Get the current full URL
+	 *
+	 * @param      <type>   $s                   { parameter_description }
+	 * @param      boolean  $use_forwarded_host  The use forwarded host
+	 *
+	 * @return     string   ( description_of_the_return_value )
+	 */
+	static function full_url($s, $use_forwarded_host = false) {
+	    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+	    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+	    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+	    $port     = $s['SERVER_PORT'];
+	    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+	    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+	    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+	    return $protocol . '://' . $host . $s['REQUEST_URI'];
+	}
 }
