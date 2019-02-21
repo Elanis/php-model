082d6415482467b146a9b8130ee2433be01f51ff
Elanis <elanis@hotmail.com>
2019-02-21 14:20:27 +0100

    Updated default site configuration

--- config.php
+++ config.php
@@ -1,11 +1,20 @@
 <?php
 $config = array(
-	"mongoURL" => "",
-	"languageList" => [
+	'mongoURL' => '',
+	'SQLCredentials' => [
+		'host' => '',
+		'db' => '',
+		'id' => '',
+		'password' => '',
+		'driver' => 'pgsql', // Ou mysql
+		'port' => 5432 // Ou 3306 si mysql
+
+	],
+	'languageList' => [
 		'en' => 'English',
 		'fr' => 'Francais',
 		//'de' => 'Deutsch',
 		//'ru' => 'русский'
 	],
-	"cacheBlacklist" => []
+	'cacheBlacklist' => [ ]
 );
